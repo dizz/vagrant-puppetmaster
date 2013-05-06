9d0968fb6f87d7192ee211f3a35ebe56224f8c71
Andy Edmonds <andy@edmonds.be>
2013-05-06 23:03:04 +0200

    fixes Puppetfile problem

--- setup_puppet_modules.sh
+++ setup_puppet_modules.sh
@@ -2,8 +2,8 @@
 
 # install the modules for the puppet master install
 cd puppet_master_src
-./get_modules.sh
+librarian-puppet install
 
 # install the modules for the wordpress install
 cd ../all_src
-./get_modules.sh
+librarian-puppet install
