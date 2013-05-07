56e3af7bd41728f0c957d640a1c43b6ea24fb231
Andy Edmonds <andy@edmonds.be>
2013-05-07 13:44:40 +0200

    added basic AIO class declaration

--- all_src/manifests/site.pp
+++ all_src/manifests/site.pp
@@ -1,1 +1,41 @@
-import "classes/*"
+# import "classes/*"
+
+node /osaio.cloudcomplab.dev/ {
+
+	# TODO apt-get install ubuntu-cloud-keyring
+	# TODO /etc/apt/sources.list.d/grizzly.list
+	# TODO deb http://ubuntu-cloud.archive.canonical.com/ubuntu precise-updates/grizzly main
+	# TODO apt-get update
+
+	include 'apache'
+
+	# it's likely that this class is no longer 100% with quantum
+	class { 'openstack::all':
+		public_address          => $ipaddress_eth1,
+		public_interface        => 'eth1',
+		private_interface       => 'eth2',
+		admin_email             => 'god@hell.io',
+		admin_password          => 'dontask',
+		secret_key				=> 'dontask',
+		mysql_root_password		=> 'mysequel2.0',
+		keystone_db_password    => 'stpeter',
+		keystone_admin_token    => 'stpeter',
+		nova_db_password        => 'nero',
+		nova_user_password      => 'nero',
+		glance_db_password      => 'gluncie',
+		glance_user_password    => 'glunce',
+		rabbit_password         => 'zaio-baio',
+		rabbit_user             => 'zaiche',
+		libvirt_type            => 'qemu',
+		floating_range          => '192.168.56.64/28',
+		fixed_range             => '10.0.0.0/24',
+		verbose                 => true,
+		auto_assign_floating_ip => false
+	}
+
+	class { 'openstack::auth_file':
+		admin_password       => 'dontask',
+		keystone_admin_token => 'stpeter',
+		controller_node      => '127.0.0.1',
+	}
+}
