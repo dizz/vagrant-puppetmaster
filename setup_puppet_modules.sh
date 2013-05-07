1d148b2c8b6520ce258f09afd4344638609d279a
Andy Edmonds <andy@edmonds.be>
2013-05-07 13:48:34 +0200

    minor mods

--- setup_puppet_modules.sh
+++ setup_puppet_modules.sh
@@ -3,3 +3,7 @@
 # install the modules for the puppet master install
 cd puppet_master_src
 librarian-puppet install
+
+# install the modules for the openstack AIO install
+cd all_src
+librarian-puppet install
